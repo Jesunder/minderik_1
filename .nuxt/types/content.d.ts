d74e67a54b6551e3dc6a03925d1745959c63c99b
Jesunder <sergeeverik7@gmail.com>
2024-10-17 16:03:25 +0200

    update ignore

---
+++ .nuxt/types/content.d.ts
@@ -0,0 +1,4 @@
+declare module '#content/server' {
+  const serverQueryContent: typeof import('C:/Users/serge/OneDrive/Documents/GitHub/minderik_1/node_modules/@nuxt/content/dist/runtime/legacy/types').serverQueryContent
+  const parseContent: typeof import('C:/Users/serge/OneDrive/Documents/GitHub/minderik_1/node_modules/@nuxt/content/dist/runtime/server').parseContent
+}
